903493f0276316c142faa60d613fb695e360f52d
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2020-03-05 15:44:25 +0000

    Fix missing comma in setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
