54e1505b6d0ec83f44b4b4f9e9a41b8c053eaea2
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2020-03-06 17:06:40 +0000

    Bump version to 0.1.7b1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ def find_packages() -> List[str]:
 
 setup(
     name="PyMODAlib",
-    version="0.1.5b4",
+    version="0.1.7b1",
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=requirements,
