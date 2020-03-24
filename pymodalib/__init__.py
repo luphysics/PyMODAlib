24d9fd59a0f8bf7e3ee16eeecda61dd17f5f2da4
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2020-03-24 12:41:26 +0000

    Move cache functions to cache.py

--- pymodalib/__init__.py
+++ pymodalib/__init__.py
@@ -13,59 +13,9 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
+from utils.cache import cachedarray, cleanup
 
-import os
-import uuid
-import warnings
-from os.path import join
-from pathlib import Path
-from typing import Tuple
-
-import numpy as np
-from numpy import ndarray
-
-CACHE_FOLDER = os.environ.get("PYMODALIB_CACHE") or join(f"{Path.home()}", ".pymodalib")
-os.makedirs(CACHE_FOLDER, exist_ok=True)
-
-ssd_warning = (
-    f"The cache folder is located at {CACHE_FOLDER}. It is highly recommended that you ensure the "
-    f"cache folder is not located on an SSD. For more information, please see the developer guide."
+__imported = (
+    cachedarray,
+    cleanup,
 )
-warnings.filterwarnings("once", "The cache folder is located at")
-
-
-def __generate_name() -> str:
-    """
-    Generates a random name for a cache file, using a UUID.
-    """
-    return f"{uuid.uuid4()}.dat"
-
-
-def cachedarray(shape: Tuple[int, ...], dtype) -> ndarray:
-    """
-    Creates a Numpy 'memmap' array which is able to cache its values to disk.
-
-    :param shape: the shape of the array
-    :param dtype: the data type of the array
-    :return: a 'memmap' array
-    """
-    warnings.warn(ssd_warning, ResourceWarning)
-
-    name = __generate_name()
-    while name in os.listdir(CACHE_FOLDER):
-        name = __generate_name()
-
-    filename = join(CACHE_FOLDER, name)
-    return np.memmap(filename, shape=shape, dtype=dtype, mode="w+")
-
-
-def cleanup() -> None:
-    """
-    Deletes all files in the cache folder.
-    """
-    for file in os.listdir(CACHE_FOLDER):
-        if file.endswith(".dat"):
-            try:
-                os.remove(join(CACHE_FOLDER, file))
-            except Exception as e:
-                print(e)
