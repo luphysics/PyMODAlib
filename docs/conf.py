b4cffa11be4813917e6f20ef0aa773644e4c72d8
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2020-03-25 15:59:22 +0000

    Fix 'master_doc' in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,8 @@ author = "Pavle Boškoski; Dmytro Iatsenko; Gemma Lancaster; Sam McCormack; Juli
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = "index"
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
